63229ec9e39efb8114d8134e1f2fb389caefc0e5
Jack01638 <79591906+Jack01638@users.noreply.github.com>
2023-10-29 18:28:04 +0000

    filefunction comments

--- BodyTracking/filefunctions.py
+++ BodyTracking/filefunctions.py
@@ -1,9 +1,10 @@
 ### Functions for reading writing and deleting data file
 
+
 ### Check for saved file
 def CheckFile():
-    #check if data.txt in "data" folder
-    #if in folder, return true
+    # check if data.txt in "data" folder
+    # if in folder, return true
     found = True
     return found
 
@@ -11,50 +12,51 @@ def CheckFile():
 ### Read file into dict from data.txt
 def ReadFile():
     #read line by line:
-        #store each line in 2d array
-        #[1 if exercise being used,exercise name, total sets - 1, total reps]
-    return 0 #return array with file data
+        # store each line in 2d array
+        # [1 if exercise being used,exercise name, total sets - 1, total reps]
+    return 0 # return array with file data
 
 
-###display the file array thats been read in from data.txt
+### Display the file array thats been read in from data.txt
 def ViewFile():
-    #only view if been read in
-    #print list in following format:
+    # only view if been read in
+    # print list in following format:
     '''
     Exercise Name - Sets + 1 / Reps
     ---------------------------
     Bicep Curl - 10 / 12
     Row - 8 / 15
     '''
-    return 0 #keep returning 0 as nothing needs to be returned
+    return 0 # keep returning 0 as nothing needs to be returned
 
 
-###Load in and use the data file
+### Load in and use the data file
 def LoadFile():
     # using array from ReadFile(), load each exercuse into the dictinary
     # make sure this function is called before we make a deep copy, or make a new deep copy of the new one after so ensure rep_dict_info is correct
     return 0
 
 
-###Edit data file
+### Edit data file
 def EditFile():
-    #only call if confirmed file exists
-    #call ViewFile() to show data
-    #while still editing:
-        #select which element in array (the exercise) you want to edit
-            #print old data for this exercise
-            #enter value to change sets to
-            #enter value to change reps to
-            #change entry in the array
-            #print confirmed new data for this exercise
-        #call ViewFile()
-    #confirm to save these changes to file (cannot be undone)
-    #save array to data.txt and overwrite old data
+    # only call if confirmed file exists
+    # call ViewFile() to show data
+    # while still editing:
+        # select which element in array (the exercise) you want to edit
+            # print old data for this exercise
+            # enter value to change sets to
+            # enter value to change reps to
+            # change entry in the array
+            # print confirmed new data for this exercise
+        # call ViewFile()
+    # confirm to save these changes to file (cannot be undone)
+    # save array to data.txt and overwrite old data
     return 0
 
+### Delete saved file
 def DeleteFile():
-    #only do if file is made
-    #ViewFile()
-    #confirm deltetion
-    #set data.txt to empty sets/reps
+    # only do if file is made
+    # ViewFile()
+    # confirm deltetion
+    # set data.txt to empty sets/reps
     return 0
